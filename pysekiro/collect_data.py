e4a97654b590717a4ce95c1da91d547df51c80af
ricagj <lcj8734612801@gmail.com>
2021-03-06 15:46:10 +0800

    Update

--- pysekiro/collect_data.py
+++ pysekiro/collect_data.py
@@ -22,44 +22,41 @@ def get_output(keys):    # 对按键信息进行独热编码
     output = [0, 0, 0, 0, 0]
 
     if   'J' in keys:
-        output[0] = 1
+        output[0] = 1    # 等同于[1, 0, 0, 0, 0]
     elif 'K' in keys:
-        output[1] = 1
+        output[1] = 1    # 等同于[0, 1, 0, 0, 0]
     elif 'LSHIFT' in keys:
-        output[2] = 1
+        output[2] = 1    # 等同于[0, 0, 1, 0, 0]
     elif 'SPACE' in keys:
-        output[3] = 1
+        output[3] = 1    # 等同于[0, 0, 0, 1, 0]
     else:
-        output[4] = 1
+        output[4] = 1    # 等同于[0, 0, 0, 0, 1]
 
     return output
 
 class Data_collection:
-    def __init__(
-        self,
-        target
-    ):
+    def __init__(self, target):
         self.target = target    # 目标
         self.dataset = list()    # 保存数据的容器
         self.save_path = os.path.join('The_battle_memory', self.target)    # 保存的位置
         if not os.path.exists(self.save_path):    # 确保保存的位置存在
             os.mkdir(self.save_path)
         np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
-        self.reward_system = RewardSystem()
+        self.reward_system = RewardSystem()    # 奖惩系统
 
-        self.step = 0
+        self.step = 0    # 计步器
 
     def save_data(self):
         print('\n\nStop, please wait')
         n = 1
-        while True:
+        while True:    # 直到找到保存位置并保存就 break
             filename = f'training_data-{n}.npy'
             save_path = os.path.join(self.save_path, filename)
-            n += 1
-            if not os.path.exists(save_path):    # 找到不会文件名重复的位置保存
+            if not os.path.exists(save_path):    # 没有重复的文件名就执行保存并退出
                 print(save_path)
                 np.save(save_path, self.dataset)
                 break
+            n += 1
         print('Done!')
         return filename[:-4]
 
@@ -92,9 +89,13 @@ class Data_collection:
                     if t > 0:
                         time.sleep(t)
 
-                    print(f'\rstep:{self.step}. Loop took {round(time.time()-last_time, 3):>5} seconds. action: {action_map[np.argmax(action)]:>10}. Self HP: {Self_HP:>3}, Self Posture: {Self_Posture:>3}, Target HP: {Target_HP:>3}, Target Posture: {Target_Posture:>3}', end='')
+                    print(f'\rstep:{self.step:>4}. Loop took {round(time.time()-last_time, 3):>5} seconds. \
+                        action: {action_map[np.argmax(action)]:>10}. \
+                        Self HP: {Self_HP:>3}, Self Posture: {Self_Posture:>3}, Target HP: {Target_HP:>3}, Target Posture: {Target_Posture:>3}', end='')
 
                 if 'P' in keys:    # 结束，保存数据
-                    filename = self.save_data()
-                    self.reward_system.save_reward_curve(save_path='Data_quality\\'+self.target+'_'+filename+'.png')    # 绘制 reward 曲线并保存在当前目录
+                    filename = self.save_data()    # 保存数据，保存结束后返回符合条件的文件名
+                    self.reward_system.save_reward_curve(
+                        save_path = os.path.join('Data_quality', self.target, filename+'.png')
+                    )    # 绘制 reward 曲线并保存在当前目录
                     break
