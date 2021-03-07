041de3018bceee100d697cb328bcb64d41bb046c
ricagj <lcj8734612801@gmail.com>
2021-03-08 02:26:01 +0800

    Change input_shape

--- learn_online.py
+++ learn_online.py
@@ -1,11 +1,9 @@
 import time
 
-import cv2
 import numpy as np
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 
 from pysekiro.Agent import Sekiro_Agent
-from pysekiro.collect_data import action_map
 from pysekiro.get_keys import key_check
 from pysekiro.get_status import get_status
 from pysekiro.get_vertices import roi
@@ -13,8 +11,8 @@ from pysekiro.grab_screen import get_screen
 
 # ---*---
 
-x   = 190
-x_w = 290
+x   = 140
+x_w = 340
 y   = 30
 y_h = 230
 
@@ -62,7 +60,7 @@ def learn_online(model_weights=None, save_path=None):
             screen = next_screen    # 状态S
 
             if train:
-                if not (np.sum(screen == 0) > 5000 or np.sum(next_screen == 0) > 5000):    # 正常情况下不会有那么多值为0的像素点，除非黑屏了
+                if not (np.sum(screen == 0) > 32400):    # 270 * 480 / 4 = 32400 ，当图像有1/4变成黑色（像素值为0）的时候停止暂停存储数据
 
                     # ---------- store ----------
 
@@ -84,9 +82,9 @@ def learn_online(model_weights=None, save_path=None):
             if t > 0:
                 time.sleep(t)
 
-            print(f'\r{" "*100}step:{step:>4}. Loop took {round(time.time()-last_time, 3):>5} seconds. action: {action_map[action]:>10}.', end='')
+            print(f'\r{" "*100}step:{step:>4}. Loop took {round(time.time()-last_time, 3):>5} seconds.', end='')
             
-            if 'P' in keys or step == 1000:
+            if 'P' in keys:
                 if train:
                     sekiro_agent.save_evaluate_network()    # 学习完毕，保存网络权重
                 sekiro_agent.reward_system.save_reward_curve(save_path='learn_online.png')    # 绘制 reward 曲线并保存在当前目录
